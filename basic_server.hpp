8a00aba2b1a33c2ef237ea6f67603b655806b59a
Gogs <gogs@fake.local>
2016-04-26 16:50:49 +0800

    remove addtional code.

--- basic_server.hpp
+++ basic_server.hpp
@@ -43,19 +43,6 @@ private:
 		});
 	}
 
-	void do_write(std::size_t length)
-	{
-		auto self(shared_from_this());
-		boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
-			[this, self](boost::system::error_code ec, std::size_t /*length*/)
-		{
-			if (!ec)
-			{
-				do_read();
-			}
-		});
-	}
-
 	tcp::socket socket_;
 	enum { max_length = N };
 	char data_[max_length];
