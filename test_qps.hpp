50d18561e3200c7dbe5d3e063bec41041d5dffd6
Gogs <gogs@fake.local>
2016-05-04 16:15:48 +0800

    test

--- test_qps.hpp
+++ test_qps.hpp
@@ -5,40 +5,15 @@
 #include "test_bridge.h"
 #include "multi_server.hpp"
 
-TEST_CASE(test_qps)
-{
-	try
-	{
-		boost::asio::io_service io_service;
-		basic_server s(io_service, 8090);
-
-		std::thread t([&io_service] {io_service.run(); });
-		t.detach();
-
-		getchar();
-
-		std::uint64_t last_succeed_count = 0;
-
-		while (true)
-		{
-			auto curr_succeed_count = (std::uint64_t)g_succeed_count;
-			std::cout << curr_succeed_count - last_succeed_count << std::endl;
-			last_succeed_count = curr_succeed_count;
-			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-		}
-	}
-	catch (std::exception& e)
-	{
-		std::cerr << "Exception: " << e.what() << "\n";
-	}
-}
-
-//TEST_CASE(test_multi_io_service)
+//TEST_CASE(test_qps)
 //{
 //	try
 //	{
-//		multi_server server(8090, std::thread::hardware_concurrency());
-//		server.run();
+//		boost::asio::io_service io_service;
+//		basic_server s(io_service, 8090);
+//
+//		std::thread t([&io_service] {io_service.run(); });
+//		t.detach();
 //
 //		getchar();
 //
@@ -52,8 +27,33 @@ TEST_CASE(test_qps)
 //			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 //		}
 //	}
-//	catch (const std::exception& e)
+//	catch (std::exception& e)
 //	{
 //		std::cerr << "Exception: " << e.what() << "\n";
 //	}
 //}
+
+TEST_CASE(test_multi_io_service)
+{
+	try
+	{
+		multi_server server(8090, std::thread::hardware_concurrency());
+		server.run();
+
+		getchar();
+
+		std::uint64_t last_succeed_count = 0;
+
+		while (true)
+		{
+			auto curr_succeed_count = (std::uint64_t)g_succeed_count;
+			std::cout << curr_succeed_count - last_succeed_count << std::endl;
+			last_succeed_count = curr_succeed_count;
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		}
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
+}
